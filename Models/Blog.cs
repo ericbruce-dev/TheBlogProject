8051693259cc7f4ad0f4376de54983a7b374972d
EB03 <ebrucey@outlook.com>
2021-10-13 12:30:36 -0400

    Updated Author to BlogUserId

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -12,7 +12,7 @@ namespace TheBlogProject.Models
     public class Blog
     {
         public int Id { get; set; }
-        public string AuthorId { get; set; }
+        public string BlogUserId { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters", MinimumLength = 2)]
@@ -39,7 +39,7 @@ namespace TheBlogProject.Models
         public IFormFile Image { get; set; }
 
         //Navigation Property
-        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
         public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
     }
 }
