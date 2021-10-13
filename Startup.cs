8e1f081159f904c9e5782eb8341ab3a9b273cba3
EB03 <ebrucey@outlook.com>
2021-10-13 19:43:50 -0400

    Switched db from sql to postgres and migrated database

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,13 @@ namespace TheBlogProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
