cb15488e1a2a5debe9f211b8af9291badf6f4d99
EB03 <ebrucey@outlook.com>
2021-10-14 11:56:55 -0400

    Updated Startup file to include razor pages. edits to blog view and model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -39,6 +39,7 @@ namespace TheBlogProject.Models
         public IFormFile Image { get; set; }
 
         //Navigation Property
+        [Display(Name = "Author")]
         public virtual BlogUser BlogUser { get; set; }
         public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
     }
