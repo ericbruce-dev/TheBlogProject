0e10c5cfa2061c470420306467b2ea0a264b3406
Eric Bruce <35344980+ericbruce-dev@users.noreply.github.com>
2021-12-30 20:56:04 -0500

    Implement Sweet Alert and Error Handling for Tags

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -1,31 +1,57 @@
 ﻿let index = 0;
 
+
 function AddTag() {
     let tagEntry = document.getElementById("TagEntry");
+    let searchResult = Search(tagEntry.value);
 
-    if (tagEntry.value == "" || tagEntry.value == null) {
-        return;
+    if (searchResult == "") {
+        swalCustom.fire({
+            title: 'Empty!',
+            text: 'You cannot enter an empty tag',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
+
+    else if (searchResult == "duplicate")
+    {
+        swalCustom.fire({
+            title: 'Duplicate!',
+            text: 'You can not enter a duplicate tag',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
     }
 
     else {
         let newOption = new Option(tagEntry.value, tagEntry.value);
         document.getElementById("TagList").options[index++] = newOption;
-
-        //Clear TagEntry control
-        tagEntry.value = "";
     }
 
-    console.log("test");
+    //Clear TagEntry control
+    tagEntry.value = "";
 }
 
 function DeleteTag() {
     let tagCount = 1;
-    while (tagCount > 0) {
-        let tagList = document.getElementById("TagList");
-        let selectedIndex = tagList.selectedIndex;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
 
-        if (selectedIndex >= 0) {
-            tagList.options[selectedIndex] = null;
+    if (tagList.selectedIndex == -1) {
+        swalCustom.fire({
+            title: 'Tag Not Selected!',
+            text: 'You need to select a tag before deleting',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+        return true;
+    }
+
+    while (tagCount > 0) {
+
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         }
 
@@ -53,3 +79,21 @@ function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[index] = newOption;
 }
+
+function Search(str) {
+    if (str == "") {
+        return "";
+    }
+
+    var tagsElement = document.getElementById('TagList');
+    if (tagsElement) {
+        let options = tagsElement.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value == str) {
+                return "duplicate";
+            }
+        }
+    }
+}
+
+
