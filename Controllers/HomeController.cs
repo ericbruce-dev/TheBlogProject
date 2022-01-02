7c8e38500c8b740fd207f29067cdbd81360aa844
Eric Bruce <35344980+ericbruce-dev@users.noreply.github.com>
2022-01-02 16:46:27 -0500

    Revert "merge"
    This reverts commit b2ae418ed84d389f8131caa1a93cb2a7d6dde359.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,11 +8,8 @@ using System.Threading.Tasks;
 using TheBlogProject.Models;
 using TheBlogProject.Services;
 using TheBlogProject.ViewModels;
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
 using TheBlogProject.Data;
-=======
->>>>>>> parent of 40114e2 (Paged List)
 
 namespace TheBlogProject.Controllers
 {
@@ -20,11 +17,13 @@ namespace TheBlogProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogEmailSender _emailSender;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender)
+        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender, ApplicationDbContext context)
         {
             _logger = logger;
             _emailSender = emailSender;
+            _context = context;
         }
 
         public IActionResult Index()
